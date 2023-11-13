d1e9b3499110a8483a4282ec543d9501a6963ae3
Jeong ChoongRae <59129179+CHOONGRAE@users.noreply.github.com>
2023-11-13 20:13:55 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.67 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,3 +1,5 @@
 function solution(today, terms, privacies) {
-    return ((t) => privacies.reduce((acc,v,i) => new Date(v.slice(0,10)) <= t[v[11]] ? [...acc, i+1] : acc, []))((([y,m,d]) => terms.reduce((acc,v) => ({...acc,[v[0]]: new Date(y, m- +v.split(' ')[1] -1,d)}),{}))(today.split('.')))
+    const [y,m,d] = today.split('.')
+    terms = terms.reduce((acc,v) => {const [k,t]=v.split(' ');acc[k]=new Date(y,+m-1-t,d);return acc},{})
+    return privacies.reduce((acc,v,i) => (([d,k]) => new Date(d) <= terms[k])(v.split(' ')) ? [...acc,i+1]: acc,[])
 }
